e0d915236f765f5f0410a6e3c65035ec7aee4eb6
dariomtz <gusdamtzr@gmail.com>
2022-02-14 08:34:51 -0600

    Error handling with express

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
-const express = require('express');
+const express = require("express");
 
-const petsRoute = require('./routes/pet.route');
+const petsRoute = require("./routes/pet.route");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
 });
 
-app.use('/pets', petsRoute);
+app.use("/pets", petsRoute);
 
 // Handle errors with express
-// app.use........
+app.use("/pets", function (err, req, res, next) {
+  if (err.details) return res.status(400).send(err.details[0].message);
+  if (err instanceof NotFoundError) {
+    return res.status(404).send(err.message);
+  }
+  res.status(503).send("Oooops something went wrong, try again");
+});
 
 module.exports = app;
